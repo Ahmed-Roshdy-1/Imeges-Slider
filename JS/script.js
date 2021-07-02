023cabca4abfda80d87a22b432bc2759a67ab950
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-07-02 02:16:03 +0200

    push-4

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+let images = [
+    'imades/img 1',
+    'imades/img 2',
+    'imades/img 3',
+    'imades/img 4',
+    'imades/img 5'
+];
+
+let currentImage = 0;
+
+let sliderImage = document.querySelector('.slider Image');
+let nextBtn = document.querySelector('.slider-button-next');
+let prevBtn = document.querySelector('.slider-button-prev');
+let playBtn = document.querySelector('.slider-button-play');
+
+let nextImage = () => {
+    if (currentImage < images.length) {
+        currentImage++;
+    } else {
+        currentImage = 0;
+
+    }
+    sliderImage.src = images[currentImage];
+
+}
+
+nextBtn.addEventListener('click', nextImage);
